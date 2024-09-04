a6f852716e262430d7f3b105dd1201b765f4c618
ijae47 <172952282+ijae47@users.noreply.github.com>
2024-09-04 11:00:41 +0900

    9/4 Update

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,5 +1,6 @@
+from dataclasses import Field
 from typing import List, Optional
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class UserProfileResponse(BaseModel):
     user_type: int
@@ -9,11 +10,29 @@ class UserProfileResponse(BaseModel):
     age: Optional[int]
     k_age: Optional[int]
     user_lifeservice: str
-    bedtime: Optional[str]
-    wakeup_time: Optional[str]
+    bedtime: Optional[str]   # 'HH:MM' 형식의 문자열
+    wakeup_time: Optional[str]   # 'HH:MM' 형식의 문자열
     user_adjective: List[str]
     user_interest: List[str]
     user_want_adjective: List[str]
     user_biography: Optional[str]
     has_disease: str
     has_pet: str
+
+class UserProfileUpdate(BaseModel):
+    user_nickname: Optional[str] = Field(None, example = "닉네임")
+    life_service: Optional[str] = Field(None, example = "생활서비스")
+    bedtime: Optional[str] = Field(None, description = "'HH:MM' 형식의 문자열", example="23:00")  # 'HH:MM' 형식의 문자열
+    wakeup_time: Optional[str] = Field(None, description = "'HH:MM' 형식의 문자열", example="09:00") # 'HH:MM' 형식의 문자열
+    user_adjective: Optional[List[str]] = Field(None, example = ["밝은", "용감한"])
+    user_interest: Optional[List[str]] = Field(None, example = ["골프", "요가"])
+    user_want_adjective: Optional[List[str]] = Field(None, example = ["겸손한", "신중한"])
+    user_biography: Optional[str] = Field(None, example = "자기소개")
+    has_disease: Optional[bool] = Field(None, example = False)
+    has_pet: Optional[bool] = Field(None, example = False)
+
+    class Config:
+        orm_mode = True
+
+class UpdateResponseMessage(BaseModel):
+    message: str
