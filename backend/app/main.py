7b8d8bc7b445a070a1081c83c7d5e57576c1d64b
ijae47 <172952282+ijae47@users.noreply.github.com>
2024-09-04 11:01:27 +0900

    9/4 Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
-from app.api.endpoints import user
+from app.api.endpoints import user, category
 
 app = FastAPI()
 
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
 # 라우터 등록
-app.include_router(user.router, prefix="/api", tags=["user"])
+app.include_router(user.router, prefix="/users", tags=["users"])
+app.include_router(category.router, prefix="/categories", tags=["categories"])
