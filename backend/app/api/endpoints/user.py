e16ea586d98364ffd59775e04ef46159bad84be0
ijae47 <172952282+ijae47@users.noreply.github.com>
2024-09-09 01:34:03 +0900

    9/9 Update

--- backend/app/api/endpoints/user.py
+++ backend/app/api/endpoints/user.py
@@ -1,11 +1,19 @@
+from datetime import timedelta
 from fastapi import APIRouter, Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.db import crud
-from app.schemas.user import UserProfileResponse, UserProfileUpdate, UpdateResponseMessage
+from app.core.config import settings
+from jose import JWTError, jwt
+from app.db.models import User
+from app.schemas.user import UserProfileResponse, UserProfileUpdate, UpdateResponseMessage, Login
+from app.core.auth import create_access_token, get_current_user, verify_token, Token, TokenData
+from app.db.crud.user_session import get_session_by_token, delete_session
 from app.api.deps import get_db
+from app.core.config import settings
 
 router = APIRouter()
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="users/login")
 
 # 사용자 프로필 조회
 @router.get("/profile/{user_id}", response_model=UserProfileResponse, summary="프로필 조회")
@@ -20,3 +28,39 @@ def get_user_profile(user_id: int, db: Session = Depends(get_db)):
 def update_profile(user_id: int, profile_update: UserProfileUpdate, db: Session = Depends(get_db)):
     updated_user = crud.user.update_user_profile(db, user_id, profile_update)
     return updated_user
+
+
+# 로그인
+@router.post("/login", response_model=Token, summary="로그인")
+def login(login: Login, db: Session = Depends(get_db)):
+    user = db.query(User).filter(User.user_name == login.user_name).first()
+
+    if not user or not user.verify_password(login.password):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+
+    access_token_expires = timedelta(minutes=settings.ACCESS_TOKEN_EXPIRE_MINUTES)
+
+    access_token = create_access_token(
+        data={"user_id": str(user.user_id)},
+        expires_delta=access_token_expires
+    )
+
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@router.get("/me", summary="현재 로그인된 사용자 ID 가져오기")
+def get_logged_in_user_id(current_user: User = Depends(get_current_user)):
+    return {"user_id": current_user.user_id}
+
+
+# 로그아웃
+@router.post("/logout", summary="로그아웃")
+def logout(token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)):
+    # 현재 토큰에 해당하는 세션을 찾음
+    session = get_session_by_token(db, token)
+    if session:
+        # 세션 삭제
+        delete_session(db, session.session_id)
+        return {"message": "Successfully logged out"}
+    else:
+        raise HTTPException(status_code=404, detail="Session not found")
