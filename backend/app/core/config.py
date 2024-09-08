f2952fca20bece2fe1280acb308ee1d0c878c8e6
ijae47 <172952282+ijae47@users.noreply.github.com>
2024-09-09 01:36:17 +0900

    9/9 Update

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -7,6 +7,9 @@ load_dotenv()
 
 class Settings(BaseSettings):
     DATABASE_URL: str = os.getenv("DATABASE_URL")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
+    ALGORITHM: ClassVar[str] = 'HS256'
+    ACCESS_TOKEN_EXPIRE_MINUTES : int = 120
 
     class Config:
         env_file = ".env"
